c782a85d479bfcbd266cc900bbfef9f0c3c2adbb
Ryan0911 <gohenry101@gmail.com>
2021-06-05 17:57:44 +0800

    動態Menu

--- ImStudentFinal/Student/mainCourse.aspx.cs
+++ ImStudentFinal/Student/mainCourse.aspx.cs
@@ -9,10 +9,42 @@ public partial class mainCourse : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string schoolName = ReadDatabase.UserInfo(Session["userID"].ToString()).School;
         int courseId = 100001; //TODO
         lbSchoolName.Text = ReadDatabase.CourseInfo(courseId).School;
         lbCourseName.Text = ReadDatabase.CourseInfo(courseId).CourseName;
         lbTeacherName.Text = ReadDatabase.UserInfo(ReadDatabase.CourseInfo(courseId).Teacher).Name;
         lbCourseCredit.Text = ReadDatabase.CourseInfo(courseId).CourseCredit.ToString() + " 學分";
+        var courseList = ReadDatabase.ListAllCourseInSchool(schoolName); //課程清單
+        var chapterList = ReadDatabase.ListAllChapterInCourse(courseId); //章節清單 參數放課程Id
+        String html = string.Empty;
+
+        for (int i = 0; i < courseList.Count; i++)
+        {
+
+            chapterList = ReadDatabase.ListAllChapterInCourse(int.Parse(courseList[i].Value)); //章節清單 參數放課程Id
+            string num = i.ToString();
+            html += "<div class=\"accordion-item\">";
+            html += " <h2 class=\"accordion-header\" id=\"panelsStayOpen-heading" + num + "\">";
+            html += "<button class=\"accordion-button collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#panelsStayOpen-collapse" + num + "\" aria-expanded=\"true\" aria-controls=\"panelsStayOpen-collapse" + num + "\">";
+            html += "<div class=\"accordionFonts\">" + courseList[i].Text + "<br><br>共" + chapterList.Count.ToString() + "堂";
+            html += "</div>";
+            html += "</button>";
+            html += "</h2>";
+            html += "<div id=\"panelsStayOpen-collapse" + num + "\" class=\"accordion-collapse collapse\" aria-labelledby=\"panelsStayOpen-heading" + num + "\">";
+            for (int j = 0; j < chapterList.Count; j++)
+            {
+                html += "<div class=\"accordion-body\">";
+
+                html += "<div onclick=\"test()\"><strong>第" + chapterList[i].Value + "章</strong></div>";
+                html += "</div>";
+
+            }
+            html += "</div>";
+            html += "</div>";
+            this.accordionPanelsStayOpenExample.Controls.Add(new LiteralControl(html));
+
+            html = string.Empty;
+        }
     }
 }
--- ImStudentFinal/Student/selectCourse.aspx.cs
+++ ImStudentFinal/Student/selectCourse.aspx.cs
@@ -9,54 +9,7 @@ public partial class selectCourse : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (!IsPostBack)
-        {
-            LoadGridViewData();
-        }
+        
     }
 
-    private void LoadGridViewData()
-    {
-        //string schoolName = Server.UrlDecode(Request.QueryString["school"]);
-        string schoolName = "元智資管學校";
-
-        GVCourse.DataSource = ReadDatabase.CourseInSchool(schoolName);
-        GVCourse.DataBind();
-        //不顯示的colums 在此設定
-        for (int co = 1; co <= 3; co += 2)
-        {
-            GVCourse.HeaderRow.Cells[co].Visible = false;
-            for (int i = 0; i < GVCourse.Rows.Count; i++)
-            {
-                GVCourse.Rows[i].Cells[co].Visible = false;
-            }
-        }
-        //表頭名稱設定
-        GVCourse.HeaderRow.Cells[2].Text = "課程名稱";
-        GVCourse.HeaderRow.Cells[4].Text = "教授姓名";
-        GVCourse.HeaderRow.Cells[5].Text = "學分數";
-    }
-
-    protected void searchBtn_Click(object sender, EventArgs e)
-    {
-        string StrCourseName = courseName.Text.Trim();
-        string StrProfessorrName = professorName.Text.Trim();
-
-        GVCourse.DataSource = ReadDatabase.SearchCourseByWord(StrCourseName, StrProfessorrName);
-        GVCourse.DataBind();
-
-        //不顯示的colums 在此設定
-        for (int co = 1; co <= 3; co += 2)
-        {
-            GVCourse.HeaderRow.Cells[co].Visible = false;
-            for (int i = 0; i < GVCourse.Rows.Count; i++)
-            {
-                GVCourse.Rows[i].Cells[co].Visible = false;
-            }
-        }
-        //表頭名稱設定
-        GVCourse.HeaderRow.Cells[2].Text = "課程名稱";
-        GVCourse.HeaderRow.Cells[4].Text = "教授姓名";
-        GVCourse.HeaderRow.Cells[5].Text = "學分數";
-    }
 }
