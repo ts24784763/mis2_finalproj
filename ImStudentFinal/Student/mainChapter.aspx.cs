3dc20394a0c8c47adb750a0f4cea48cf08fef771
Ryan0911 <gohenry101@gmail.com>
2021-06-06 15:38:37 +0800

    解衝突

--- ImStudentFinal/Student/mainChapter.aspx.cs
+++ ImStudentFinal/Student/mainChapter.aspx.cs
@@ -12,18 +12,17 @@ public partial class subCourse : System.Web.UI.Page
         string schoolName = ReadDatabase.UserInfo(Session["userID"].ToString()).School;
         int courseId = 100001; //TODO
         var courseList = ReadDatabase.ListAllCourseInSchool(schoolName); //課程清單
-        var chapterList = ReadDatabase.ListAllChapterInCourse(courseId); //章節清單 參數放課程Id
+        var chapterList = ReadDatabase.ListAllChapterInCourse(100001); //章節清單 參數放課程Id
         String html = string.Empty;
 
         for (int i = 0; i < courseList.Count; i++)
         {
-
             chapterList = ReadDatabase.ListAllChapterInCourse(int.Parse(courseList[i].Value)); //章節清單 參數放課程Id
             string num = i.ToString();
             html += "<div class=\"accordion-item\">";
             html += " <h2 class=\"accordion-header\" id=\"panelsStayOpen-heading" + num + "\">";
             html += "<button class=\"accordion-button collapsed\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#panelsStayOpen-collapse" + num + "\" aria-expanded=\"true\" aria-controls=\"panelsStayOpen-collapse" + num + "\">";
-            html += "<div class=\"accordionFonts\">" + courseList[i].Text + "<br><br>共" + chapterList.Count.ToString() + "堂";
+            html += "<div class=\"accordionFonts\" onclick=\"course(this) \" id =\"" + courseList[i].Value + "\" >" + courseList[i].Text + "<br><br>共" + chapterList.Count.ToString() + "堂";
             html += "</div>";
             html += "</button>";
             html += "</h2>";
@@ -31,15 +30,12 @@ public partial class subCourse : System.Web.UI.Page
             for (int j = 0; j < chapterList.Count; j++)
             {
                 html += "<div class=\"accordion-body\">";
-
-                html += "<div onclick=\"test()\"><strong>第" + chapterList[i].Value + "章</strong></div>";
+                html += "<div type=\"button\" id=\"" + courseList[i].Value + ":" + chapterList[j].Value + "\" onclick=\"test(this)\"><strong>第" + chapterList[j].Value + "章</strong></div>";
                 html += "</div>";
-
             }
             html += "</div>";
             html += "</div>";
             this.accordionPanelsStayOpenExample.Controls.Add(new LiteralControl(html));
-
             html = string.Empty;
         }
     }
