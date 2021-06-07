8ccdf39b3f817da005ac9eb42fd895b1c5c1e33f
28cyc <by890228@gmail.com>
2021-06-07 23:06:15 +0800

    checkHW加一些驗證

--- ImStudentFinal/App_Code/IsExists.cs
+++ ImStudentFinal/App_Code/IsExists.cs
@@ -167,4 +167,30 @@ public class IsExists
             }
         }
     }
+
+    /// <summary>
+    /// 判斷老師是否已審核過此學生
+    /// </summary>
+    /// <param name="student"></param>
+    /// <param name="courseId"></param>
+    /// <returns></returns>
+    public static bool AlreadyCheck(string student, int courseId)
+    {
+        DataTable dt = new DataTable();
+        string sql = @"SELECT PassOrNot FROM COURSE_SELECTION WHERE Student = @Student AND CourseId = @CourseId ";
+        using (SqlConnection conn = new SqlConnection(GetDBConnectionString()))
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.Add(new SqlParameter("@Student", student));
+            cmd.Parameters.Add(new SqlParameter("@CourseId", courseId));
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+            sqlAdapter.Fill(dt);
+            conn.Close();
+        }
+        if (dt.Rows[0]["PassOrNot"].ToString() == "")
+            return false;
+        else
+            return true;
+    }
 }
--- ImStudentFinal/Teacher/checkHW.aspx.cs
+++ ImStudentFinal/Teacher/checkHW.aspx.cs
@@ -11,7 +11,6 @@ public partial class Teacher_checkHW : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         string TeacherName = Session["userID"].ToString();
-        //string TeacherName = "celesteng";
         LoadDataList();
     }
     private void LoadDataList()
@@ -23,40 +22,50 @@ public partial class Teacher_checkHW : System.Web.UI.Page
 
     protected void btnDownloadHW_Click(object sender, EventArgs e)
     {
-        string HWPath = "../Student/" + ((Button)sender).CommandArgument;
-        string HWFileName = ((Button)sender).CommandArgument.Substring(((Button)sender).CommandArgument.IndexOf("/"));
-        Response.ContentType = HWPath;
-        Response.AppendHeader("Content-Disposition", "attachment; filename=" + HWFileName);
+        string student = ((Button)sender).CommandArgument;
+        int CourseId = int.Parse(Server.UrlDecode(Request.QueryString["courseId"]));
+        if (IsExists.AlreadyUploadHW(student, CourseId))
+        {
+            string HWFilePath = ReadDatabase.CourseSelectionInfo(student, CourseId).HWFilePath;
+            string HWPath = "../Student/" + HWFilePath;
+            string HWFileName = HWFilePath.Substring(HWFilePath.IndexOf("/") + 1);
+            Response.ContentType = HWPath;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + HWFileName);
 
-        // Write the file to the Response
-        const int bufferLength = 10000;
-        byte[] buffer = new Byte[bufferLength];
-        int length = 0;
-        Stream download = null;
-        try
-        {
-            download = new FileStream(Server.MapPath(HWPath), FileMode.Open, FileAccess.Read);
-            do
+            // Write the file to the Response
+            const int bufferLength = 10000;
+            byte[] buffer = new Byte[bufferLength];
+            int length = 0;
+            Stream download = null;
+            try
             {
-                if (Response.IsClientConnected)
+                download = new FileStream(Server.MapPath(HWPath), FileMode.Open, FileAccess.Read);
+                do
                 {
-                    length = download.Read(buffer, 0, bufferLength);
-                    Response.OutputStream.Write(buffer, 0, length);
-                    buffer = new Byte[bufferLength];
+                    if (Response.IsClientConnected)
+                    {
+                        length = download.Read(buffer, 0, bufferLength);
+                        Response.OutputStream.Write(buffer, 0, length);
+                        buffer = new Byte[bufferLength];
+                    }
+                    else
+                    {
+                        length = -1;
+                    }
                 }
-                else
-                {
-                    length = -1;
-                }
+                while (length > 0);
+                Response.Flush();
+                Response.End();
             }
-            while (length > 0);
-            Response.Flush();
-            Response.End();
+            finally
+            {
+                if (download != null)
+                    download.Close();
+            }
         }
-        finally
+        else
         {
-            if (download != null)
-                download.Close();
+            Response.Write("<script>alert('" + ReadDatabase.UserInfo(student).Name + "沒上傳作業');</script>");
         }
     }
     
@@ -64,44 +73,58 @@ public partial class Teacher_checkHW : System.Web.UI.Page
     {
         string student = ((Button)sender).CommandArgument;
         int CourseId = int.Parse(Server.UrlDecode(Request.QueryString["courseId"]));
-        Models.CourseSelectionModel PassOrNot = new Models.CourseSelectionModel
+        if (IsExists.AlreadyCheck(student, CourseId))
         {
-            Student = student,
-            CourseId = CourseId,
-            PassOrNot = "通過",
-        };
-        try
-        {
-            ReadDatabase.PassOrNot(PassOrNot);
-            Response.Write("<script>alert('已讓 " + ReadDatabase.UserInfo(student).Name + " 通過 " + ReadDatabase.CourseInfo(CourseId).CourseName + "');</script>");
+            Response.Write("<script>alert('已經審核過此學生');</script>");
         }
-        catch
+        else
         {
-            Response.Write("<script>alert('審核失敗');</script>");
+            Models.CourseSelectionModel PassOrNot = new Models.CourseSelectionModel
+            {
+                Student = student,
+                CourseId = CourseId,
+                PassOrNot = "通過",
+            };
+            try
+            {
+                ReadDatabase.PassOrNot(PassOrNot);
+                Response.Write("<script>alert('已讓 " + ReadDatabase.UserInfo(student).Name + " 通過 " + ReadDatabase.CourseInfo(CourseId).CourseName + "');</script>");
+            }
+            catch
+            {
+                Response.Write("<script>alert('審核失敗');</script>");
+            }
+            LoadDataList();
         }
-        LoadDataList();
     }
+
     protected void btnDeny_Click(object sender, EventArgs e)
     {
         string student = ((Button)sender).CommandArgument;
-        //int CourseId = 100001;
         int CourseId = int.Parse(Server.UrlDecode(Request.QueryString["courseId"]));
-        Models.CourseSelectionModel PassOrNot = new Models.CourseSelectionModel
+        if (IsExists.AlreadyCheck(student, CourseId))
         {
-            Student = student,
-            CourseId = CourseId,
-            PassOrNot = "不通過",
-        };
-        try
-        {
-            ReadDatabase.PassOrNot(PassOrNot);
-            Response.Write("<script>alert('" + ReadDatabase.UserInfo(student).Name + " 不通過 " + ReadDatabase.CourseInfo(CourseId).CourseName + "');</script>");
+            Response.Write("<script>alert('已經審核過此學生');</script>");
         }
-        catch
+        else
         {
-            Response.Write("<script>alert('審核失敗');</script>");
+            Models.CourseSelectionModel PassOrNot = new Models.CourseSelectionModel
+            {
+                Student = student,
+                CourseId = CourseId,
+                PassOrNot = "不通過",
+            };
+            try
+            {
+                ReadDatabase.PassOrNot(PassOrNot);
+                Response.Write("<script>alert('" + ReadDatabase.UserInfo(student).Name + " 不通過 " + ReadDatabase.CourseInfo(CourseId).CourseName + "');</script>");
+            }
+            catch
+            {
+                Response.Write("<script>alert('審核失敗');</script>");
+            }
+            LoadDataList();
         }
-        LoadDataList();
     }
 
     protected void btnSearch_Click(object sender, EventArgs e)
@@ -119,9 +142,17 @@ public partial class Teacher_checkHW : System.Web.UI.Page
     protected void StudentHWDataList_ItemDataBound(object sender, DataListItemEventArgs e)
     {
         Label lblPassOrNot = e.Item.FindControl("lblPassOrNot") as Label;
+        Label lblHWName = e.Item.FindControl("lblHWName") as Label;
+        Button btnAllow = e.Item.FindControl("btnAllow") as Button;
+        Button btnDeny = e.Item.FindControl("btnDeny") as Button;
         if (lblPassOrNot.Text == "")
         {
             lblPassOrNot.Text = "未審核";
+        }
+        if (lblHWName.Text == "")
+        {
+            btnAllow.Enabled = false;
+            btnDeny.Enabled = false;
         }
     }
 }
